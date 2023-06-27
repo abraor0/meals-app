4a1de7dd27cf3d08fada34d5d28d12eb58dcff88
Abrão Rodrigues <arnf2710@gmail.com>
2023-06-27 04:53:45 -0300

    day 4

--- lib/filters_screen.dart
+++ lib/filters_screen.dart
@@ -2,15 +2,76 @@ import 'package:flutter/material.dart';
 
 import './widgets/app_scaffold.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _isGlutenFree = false;
+  var _isLactoseFree = false;
+  var _isVegetarian = false;
+  var _isVegan = false;
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      child: Text("hello there"),
       title: 'Filters',
       drawer: true,
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(26),
+            width: double.infinity,
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleSmall!
+                  .copyWith(fontWeight: FontWeight.bold, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SwitchListTile(
+              title: const Text('Gluten-free'),
+              subtitle: const Text('Only include gluten-free meals'),
+              value: _isGlutenFree,
+              onChanged: (value) {
+                setState(() {
+                  _isGlutenFree = value;
+                });
+              }),
+          SwitchListTile(
+              title: const Text('Lactose-free'),
+              subtitle: const Text('Only include lactose-free meals'),
+              value: _isLactoseFree,
+              onChanged: (value) {
+                setState(() {
+                  _isLactoseFree = value;
+                });
+              }),
+          SwitchListTile(
+              title: const Text('Vegetarian'),
+              subtitle: const Text('Only include vegetarian meals'),
+              value: _isVegetarian,
+              onChanged: (value) {
+                setState(() {
+                  _isVegetarian = value;
+                });
+              }),
+          SwitchListTile(
+              title: const Text('Vegan'),
+              subtitle: const Text('Only include vegan meals'),
+              value: _isVegan,
+              onChanged: (value) {
+                setState(() {
+                  _isVegan = value;
+                });
+              })
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/category_meals_screen.dart';
+import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/favorites_screen.dart';
+import 'package:meals_app/models/IMeal.dart';
 import 'package:meals_app/tab_screen.dart';
+import 'package:meals_app/widgets/meal_detail.dart';
 
 import './tab_screen.dart';
 
@@ -7,14 +13,50 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false
+  };
+
+  List<IMeal> _availableMeals = DUMMY_MEALS;
+
+  void setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+    });
+
+    _availableMeals = DUMMY_MEALS.where((meal) {
+      if (_filters['gluten']! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_filters['lactose']! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_filters['vegan']! && !meal.isVegan) {
+        return false;
+      }
+      if (_filters['vegetarian']! && !meal.isVegetarian) {
+        return false;
+      }
+
+      return true;
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meals App',
-      home: Home(),
       theme: ThemeData(
         fontFamily: 'Raleway',
         colorSchemeSeed: Colors.orange,
@@ -28,15 +70,14 @@ class MyApp extends StatelessWidget {
               fontSize: 22,
             )),
       ),
+      routes: {
+        '/': (context) => TabScreen(),
+        '/categories': (context) => CategoriesScreen(),
+        '/category': (context) =>
+            CategoryMealsScreen(categoryId: categoryId, title: title),
+        '/favorites': (context) => FavoritesScreen(),
+        '/meal-detail': (context) => MealDetail(),
+      },
     );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TabScreen();
   }
 }
